68ec42ea092ea9c7a86fc45da75c19a9c8c67613
KARAN GUPTA <karan.20gcebcs103@galgotiacollege.edu>
2024-04-11 22:05:10 +0530

    Create testProblem3.js

---
+++ TestProblem_Car/testProblem3.js
@@ -0,0 +1,6 @@
+import sortCarModels from '../Problem_Car/Problem3.js';
+import inventory from '../Cars_Inventory/inventory.js';
+
+
+const sortedModels = sortCarModels(inventory);
+console.log(sortedModels);
