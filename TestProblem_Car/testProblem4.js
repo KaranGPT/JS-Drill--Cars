34b238109a7a39286437a786507e7903bcd6423e
KARAN GUPTA <karan.20gcebcs103@galgotiacollege.edu>
2024-04-11 22:16:10 +0530

    Create testProblem4.js

---
+++ TestProblem_Car/testProblem4.js
@@ -0,0 +1,7 @@
+import carYears from '../Problem_Car/Problem4.js';
+import inventory from '../Car_Inventory/inventory.js';
+
+
+const carYearsList = carYears(inventory);
+
+console.log(carYearsList);
